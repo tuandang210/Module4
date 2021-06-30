11e3ddbb7d9b02e468fd57c0993e36831429f42b
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-06-30 14:20:06 +0700

    [Thực hành] Sử dụng Spring Security

---
+++ SpringSecurity/securityLoginForm/src/main/java/com/codegym/configuration/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.codegym.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder a) throws Exception {
+        a.inMemoryAuthentication()
+                .withUser("user").password("{noop}123456").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}123456").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/admin**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+}
